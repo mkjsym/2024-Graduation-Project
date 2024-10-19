bc47ce0d28336496e5c3aaaed019fba5c066b89d
mkjsym <mkjsym@naver.com>
2024-10-19 16:57:26 +0900

    added smtp.py

---
+++ Sources/smtp.py
@@ -0,0 +1,17 @@
+import smtplib
+from email.mime.text import MIMEText
+
+#use port 587 or 465
+smtp = smtplib.SMTP('smtp.gmail.com', 587)
+
+smtp.ehlo()
+smtp.starttls()
+
+smtp.login('mkjsym@gmail.com', 'itof hbrd duzh vwjw')
+
+msg = MIMEText('content: test')
+msg['Subject'] = 'title: test msg'
+
+smtp.sendmail('mkjsym@gmail.com', 'mkjsym@naver.com', msg.as_string())
+
+smtp.quit()
